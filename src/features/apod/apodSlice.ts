34342c12151581125225b77818714283357f3dd4
Kim Min Sung <1nnovator1105@gmail.com>
2024-08-15 14:52:25 +0900

    feat: connect vercel

--- src/features/apod/apodSlice.ts
+++ src/features/apod/apodSlice.ts
@@ -1,104 +1,109 @@
-import axios from 'axios';
-import moment from 'moment';
+import axios from "axios";
+import moment from "moment";
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type DataProps = {
-    targetDate: string,
-    isLoad: boolean,
-    data: {
-        copyright: string,
-        date: string,
-        explanation: string,
-        hdurl: string,
-        media_type: string | null,
-        service_version: string | null,
-        title: string | null,
-        url: string,
-        translate_title: string | null,
-        translate_explanation: string | null
-    },
-    errCount: number,
-    minDate: string,
-    maxDate: string
-}
+  targetDate: string;
+  isLoad: boolean;
+  data: {
+    copyright: string;
+    date: string;
+    explanation: string;
+    hdurl: string;
+    media_type: string | null;
+    service_version: string | null;
+    title: string | null;
+    url: string;
+    translate_title: string | null;
+    translate_explanation: string | null;
+  };
+  errCount: number;
+  minDate: string;
+  maxDate: string;
+};
 
-let initialState:DataProps = {
-    targetDate: moment(new Date()).utc().format('YYYY-MM-DD'),
-    isLoad: false,
-    data: {
-        copyright: "",
-        date: "",
-        explanation: "",
-        hdurl: "",
-        media_type: null,
-        service_version: null,
-        title: null,
-        url: "https://luvlog-bucket.s3.ap-northeast-2.amazonaws.com/static/n_bg.png",
-        translate_title: null,
-        translate_explanation: null
-    },
-    errCount: 0,
-    minDate: '1995-06-16',
-    maxDate: moment(new Date()).utc().add(1, 'days').format('YYYY-MM-DD')
-}
+let initialState: DataProps = {
+  targetDate: moment(new Date()).utc().format("YYYY-MM-DD"),
+  isLoad: false,
+  data: {
+    copyright: "",
+    date: "",
+    explanation: "",
+    hdurl: "",
+    media_type: null,
+    service_version: null,
+    title: null,
+    url: "https://luvlog-bucket.s3.ap-northeast-2.amazonaws.com/static/n_bg.png",
+    translate_title: null,
+    translate_explanation: null,
+  },
+  errCount: 0,
+  minDate: "1995-06-16",
+  maxDate: moment(new Date()).utc().add(1, "days").format("YYYY-MM-DD"),
+};
 
 export const apodSlice = createSlice({
-    name: 'apod',
-    initialState: initialState,
-    reducers: {
-        ADD_DAY(state) {
-            let nextDate = moment(state.targetDate).add(1, 'days').format('YYYY-MM-DD');
-            if (moment(nextDate).isBefore(state.maxDate)) {
-                state.targetDate = nextDate;
-            }
-        },
-        SUB_DAY(state) {
-            let prevDate = moment(state.targetDate).subtract(1, 'days').format('YYYY-MM-DD');
-            if (moment(prevDate).isAfter(state.minDate)) {
-                state.targetDate = prevDate;
-            }
-        },
-        LOAD_STATUS_TOGGLE(state, action: PayloadAction<boolean>) {
-            state.isLoad = action.payload;
-        }
+  name: "apod",
+  initialState: initialState,
+  reducers: {
+    ADD_DAY(state) {
+      let nextDate = moment(state.targetDate)
+        .add(1, "days")
+        .format("YYYY-MM-DD");
+      if (moment(nextDate).isBefore(state.maxDate)) {
+        state.targetDate = nextDate;
+      }
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(FETCH_DATA.pending, (state) => {
-                state.data = initialState.data;
-                state.isLoad = false;
-            })
-            .addCase(FETCH_DATA.fulfilled, (state, action) => {
+    SUB_DAY(state) {
+      let prevDate = moment(state.targetDate)
+        .subtract(1, "days")
+        .format("YYYY-MM-DD");
+      if (moment(prevDate).isAfter(state.minDate)) {
+        state.targetDate = prevDate;
+      }
+    },
+    LOAD_STATUS_TOGGLE(state, action: PayloadAction<boolean>) {
+      state.isLoad = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(FETCH_DATA.pending, (state) => {
+        state.data = initialState.data;
+        state.isLoad = false;
+      })
+      .addCase(FETCH_DATA.fulfilled, (state, action) => {
+        if (action.payload && action.payload.media_type === "image") {
+          state.data = action.payload;
+        } else {
+          if (state.errCount++ <= 5) {
+            // 오류로 인한 무제한 요청 방지
+            let prevDate = moment(state.targetDate)
+              .subtract(1, "days")
+              .format("YYYY-MM-DD");
+            state.targetDate = prevDate;
+          }
+        }
+      });
+  },
+});
 
-                if (action.payload && action.payload.media_type === 'image') {
-                    state.data = action.payload;
-                } else {
-                    
-                    if (state.errCount++ <= 5) { // 오류로 인한 무제한 요청 방지
-                        let prevDate = moment(state.targetDate).subtract(1, 'days').format('YYYY-MM-DD');
-                        state.targetDate = prevDate;
-                    }
-                    
-                }
-                
-            })
-    }
-})
-
-export let FETCH_DATA = createAsyncThunk('API/FETCH_DATA',async (_targetDate: string) => {
+export let FETCH_DATA = createAsyncThunk(
+  "API/FETCH_DATA",
+  async (_targetDate: string) => {
     // console.log(`http://localhost:3500/${_targetDate}`);
     return axios({
-        method: "get",
-        url: `http://luvlog.xyz:3500/woozooserver/${_targetDate}`
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}/woozooserver/${_targetDate}`,
     })
-    .then(res => {
-        return res.data
-    })
-    .catch(() => {
-        console.log('데이터 조회에 실패하였습니다.');
-    })
-
-})
+      .then((res) => {
+        return res.data;
+      })
+      .catch(() => {
+        console.log("데이터 조회에 실패하였습니다.");
+      });
+  }
+);
 
 export let { ADD_DAY, SUB_DAY, LOAD_STATUS_TOGGLE } = apodSlice.actions;
 
